f6a55a37370c3caea7cfc8208129e94a2841ad2d
legende0512@gmail.com <legende0512@gmail.comgit config --global user.email legende0512@gmail.com>
2020-09-27 16:56:08 +0300

    todolist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'homework';
+  title = 'Todo list';
+  tacks = [{
+    title: 'Homework 21',
+    text: 'JS. ООП',
+    status: false
+  }, {
+    title: 'Homework 22',
+    text: 'Gulp',
+    status: true
+  }, {
+    title: 'Homework 23',
+    text: 'Angular 1',
+    status: true
+  }, {
+    title: 'Homework 24',
+    text: 'TypeScript',
+    status: false
+  }]
 }
